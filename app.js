0dd243248a82d88085b7c1cdc753ca79497dec94
Jaikesh Kain <164343591+Jaikeshkain@users.noreply.github.com>
2025-05-18 15:28:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const app=express()
 const server=http.createServer(app)
 const io=socketio(server,{
     cors:{
-        origin:["http://localhost:5173"],
+        origin:["https://mern-chatapp-0110.netlify.app"],
         methods:["GET","POST"],
         credentials:true,
     }
